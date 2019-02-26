d49f1fbc6f0a56d6a159a61671c8d3cbb11dab49
Alexander Lyon <alyon011@gmail.com>
2019-02-25 21:29:47 -0500

    Setup postcss with Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,22 @@ module.exports = (grunt) => {
           'src/css/style.css': 'src/css/style.scss'
         }
       }
+    },
+    postcss: {
+      options: {
+        map: true, // inline sourcemaps
+        processors: [
+          require('autoprefixer')({browsers: 'last 2 versions'})
+        ]
+      },
+      dist: {
+        src: 'src/css/*.css'
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-sass');  
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+
+  grunt.registerTask('css', ['sass', 'postcss']);
 };
